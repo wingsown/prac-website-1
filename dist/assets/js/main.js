e98c432512f53c9b0e3947fb8c79679ab47fe37d
Wilson <wj.buena@gmail.com>
2022-06-01 18:58:11 +0800

    New section

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -23,3 +23,13 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*================ CHANGE BG HEADER ================ */
+function scrollHeader() {
+    const header = document.getElementById('header');
+    
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader);
